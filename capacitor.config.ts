c104efd1b2b23a458b12e67a4e7391fb54611016
laclauderie <jclonana2005@gmail.com>
2024-08-21 18:45:07 +0100

    Initial commit msg

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-learning-ionic',
+  webDir: 'www'
+};
+
+export default config;
